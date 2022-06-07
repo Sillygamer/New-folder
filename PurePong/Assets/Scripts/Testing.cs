f95e3625d979b313752eac94866ce596b0f2804e
MUKILTEO\1703682 <1703682@Mukilteo.wednet.edu>
2022-06-07 13:05:46 -0700

    first  video finished

--- PurePong/Assets/Scripts/Testing.cs
+++ PurePong/Assets/Scripts/Testing.cs
@@ -6,18 +6,25 @@ public class Testing : MonoBehaviour
 {
     private Rect testbox;
     private Color c;
+    private int xpos, ypos;
+
 
     // Start is called before the first frame update
     void Start()
     {
         testbox = new Rect(10, 10, 100, 200);
         c = new Color(1, 0.5f, 0);
+        xpos = 0;
+        ypos = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        xpos++;
+        ypos++;
+        testbox.x = xpos;
+        testbox.y = ypos;
     }
     private void OnGUI()
     {
