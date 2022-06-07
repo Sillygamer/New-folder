efdf5c8b66ff330ea9baa3fccccc7c6b91843ade
MUKILTEO\1703682 <1703682@Mukilteo.wednet.edu>
2022-06-07 12:42:34 -0700

    pong started

---
+++ PurePong/Assets/Scripts/Manager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Manager : MonoBehaviour
+{
+    private static Texture2D _staticRectTexture;
+    private static GUIStyle _staticRectStyle;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public static void GUIDrawRect(Rect position, Color color)
+    {
+        if(_staticRectTexture == null)
+        {
+            _staticRectTexture = new Texture2D(1, 1);
+        }
+
+        if(_staticRectStyle == null)
+        {
+            _staticRectStyle = new GUIStyle();
+            _staticRectStyle.fontSize = 16;
+        }
+
+        _staticRectTexture.SetPixel(0, 0, color);
+        _staticRectTexture.Apply();
+        _staticRectStyle.normal.background = _staticRectTexture;
+        GUI.Box(position, GUIContent.none, _staticRectStyle);
+    }//end of draw rect
+}
---
+++ PurePong/Assets/Scripts/Testing.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Testing : MonoBehaviour
+{
+    private Rect testbox;
+    private Color c;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        testbox = new Rect(10, 10, 100, 200);
+        c = new Color(1, 0.5f, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnGUI()
+    {
+        Manager.GUIDrawRect(testbox, c);
+    }
+}
