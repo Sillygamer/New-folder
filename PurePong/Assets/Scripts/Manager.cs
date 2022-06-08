ca7c771234941e6f73a1bb2f958d6c3eb4ba1afd
MUKILTEO\1703682 <1703682@Mukilteo.wednet.edu>
2022-06-08 11:49:16 -0700

    vid3 done

--- PurePong/Assets/Scripts/Manager.cs
+++ PurePong/Assets/Scripts/Manager.cs
@@ -4,13 +4,24 @@ using UnityEngine;
 
 public class Manager : MonoBehaviour
 {
+
+    //boundaries
+    public static Prect lowerWall;
+    public static Prect upperWall;
+    public static Prect centerLine;
+    private Color wallColor = new Color(1, 0, .5f, 1);
+    private Color centerLineColor = new Color(0, 1, 1, 0.5f);
+
+    //gui.box
     private static Texture2D _staticRectTexture;
     private static GUIStyle _staticRectStyle;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        upperWall = new Prect("Upper Wall", 0, 0, Screen.width, 10, true, "WALL");
+        lowerWall = new Prect("Lower Wall", 0, Screen.height - 10, Screen.width, 10, true, "WALL");
+        centerLine = new Prect("Center Wall", Screen.width / 2, 0, 5, Screen.height, false, "CTRLine");
     }
 
     // Update is called once per frame
@@ -37,4 +48,10 @@ public class Manager : MonoBehaviour
         _staticRectStyle.normal.background = _staticRectTexture;
         GUI.Box(position, GUIContent.none, _staticRectStyle);
     }//end of draw rect
+    private void OnGUI()
+    {
+        GUIDrawRect(centerLine.rect, centerLineColor);
+        GUIDrawRect(upperWall.rect, wallColor);
+        GUIDrawRect(lowerWall.rect, wallColor);
+    }
 }
---
+++ PurePong/Assets/Scripts/Prect.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Prect 
+{
+    public string name;
+    public Rect rect;
+    public int x, y, w, h;
+    public bool hasColision;
+    public string tag;
+
+    //default
+    public Prect()
+    {
+    }
+
+    public Prect(string NAME, int X, int Y, int W, int H, bool HASCOLISION, string TAG)
+    {
+        this.name = NAME;
+        this.x = X;
+        this.y = Y;
+        this.w = W;
+        this.h = H;
+        this.hasColision = HASCOLISION;
+        this.tag = TAG;
+        rect.x = x;
+        rect.y = y;
+        rect.width = w;
+        rect.height = h;
+
+    }
+}
