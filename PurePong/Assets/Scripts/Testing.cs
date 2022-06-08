1151c421f96ac0a42015e02add2efd3b12602064
MUKILTEO\1703682 <1703682@Mukilteo.wednet.edu>
2022-06-08 11:30:47 -0700

    vid2 done

--- PurePong/Assets/Scripts/Testing.cs
+++ PurePong/Assets/Scripts/Testing.cs
@@ -4,30 +4,26 @@ using UnityEngine;
 
 public class Testing : MonoBehaviour
 {
-    private Rect testbox;
+    private Prect p;
     private Color c;
-    private int xpos, ypos;
+
 
 
     // Start is called before the first frame update
     void Start()
     {
-        testbox = new Rect(10, 10, 100, 200);
+        p = new Prect("PrimRect", 40, 40, 100, 100, false, "simple");
         c = new Color(1, 0.5f, 0);
-        xpos = 0;
-        ypos = 0;
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        xpos++;
-        ypos++;
-        testbox.x = xpos;
-        testbox.y = ypos;
+
     }
     private void OnGUI()
     {
-        Manager.GUIDrawRect(testbox, c);
+        Manager.GUIDrawRect(p.rect, c);
     }
 }
