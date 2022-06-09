ac34851c5c5ba901474379c11efd3ab21e4eccad
MUKILTEO\1703682 <1703682@Mukilteo.wednet.edu>
2022-06-09 11:45:54 -0700

    vid4 done

---
+++ PurePong/Assets/Scripts/leftPaddle.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class leftPaddle : MonoBehaviour
+{
+    public float scaler = 2;
+    public Prect paddleL;
+    private Color LpaddleColor = new Color(1, 1, 0, 1);
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        paddleL = new Prect("Left Paddle", 50, 100, 10, 30, true, "PADDLE");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        paddleL.rect.y += -scaler * Input.GetAxis("Vertical");
+        if (paddleL.rect.y < 10) paddleL.rect.y = 10;
+        if (paddleL.rect.y > Screen.height - 40) paddleL.rect.y = Screen.height - 40;
+    }
+
+    private void OnGUI()
+    {
+        Manager.GUIDrawRect(paddleL.rect, LpaddleColor);
+    }
+}
---
+++ PurePong/Assets/Scripts/rightPaddle.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rightPaddle : MonoBehaviour
+{
+    public float scaler = 2;
+    public Prect paddleR;
+    private Color RpaddleColor = new Color(1, 1, 0, 1);
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        paddleR = new Prect("Right Paddle",700, 100, 10, 30, true, "PADDLE");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        paddleR.rect.y += -scaler * Input.GetAxis("Vertical");
+        if (paddleR.rect.y < 10) paddleR.rect.y = 10;
+        if (paddleR.rect.y > Screen.height - 40) paddleR.rect.y = Screen.height - 40;
+    }
+
+    private void OnGUI()
+    {
+        Manager.GUIDrawRect(paddleR.rect, RpaddleColor);
+    }
+}
